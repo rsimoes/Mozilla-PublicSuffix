e00ac1c4dcf581cb73b905f5317abe4a7ced2fef
Richard Simões <rsimoes@cpan.org>
2012-06-28 08:41:51 -0500

    untabified and readded excluded notabstest

--- lib/Mozilla/PublicSuffix.pm
+++ lib/Mozilla/PublicSuffix.pm
@@ -15,30 +15,30 @@ our @EXPORT_OK = qw(public_suffix);
 
 my $dn_re = qr/^$RE{net}{domain}$/;
 sub public_suffix {
-	my $domain = lc $_[0];
-	index($domain, "xn--") != -1
-		and $domain = eval { URI::_idna::decode($_[0]) };
-	# Test domain well-formedness:
-	$domain =~ $dn_re
-		or croak("Argument passed is not a well-formed domain name");
-	return _find_rule($domain, substr($domain, index($domain, ".") + 1 ) ) }
+    my $domain = lc $_[0];
+    index($domain, "xn--") != -1
+        and $domain = eval { URI::_idna::decode($_[0]) };
+    # Test domain well-formedness:
+    $domain =~ $dn_re
+        or croak("Argument passed is not a well-formed domain name");
+    return _find_rule($domain, substr($domain, index($domain, ".") + 1 ) ) }
 
 my %rules = qw();
 sub _find_rule {
-	my ($domain, $rhs) = @_;
-	my $drule = $rules{$domain};
-	return defined $drule       # Test for rule with full domain
-		? $drule eq "w"
-			? undef             # Wildcard rules need an extra level.
-			: $domain
-		: $domain eq $rhs
-			? undef
-			: do {
-				my $rrule = $rules{$rhs};
-				defined $rrule  # Test for rule with right-hand side
-					? $rrule eq "w"
-						? $domain
-						: $rhs
-					: _find_rule($rhs, substr($rhs, index($rhs, ".") + 1 ) ) } }
+    my ($domain, $rhs) = @_;
+    my $drule = $rules{$domain};
+    return defined $drule       # Test for rule with full domain
+        ? $drule eq "w"
+            ? undef             # Wildcard rules need an extra level.
+            : $domain
+        : $domain eq $rhs
+            ? undef
+            : do {
+                my $rrule = $rules{$rhs};
+                defined $rrule  # Test for rule with right-hand side
+                    ? $rrule eq "w"
+                        ? $domain
+                        : $rhs
+                    : _find_rule($rhs, substr($rhs, index($rhs, ".") + 1 ) ) } }
 
 1;
